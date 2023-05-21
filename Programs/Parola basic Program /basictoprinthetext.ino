2eb71e27d82cec9c377e1ac494626903dd65a3aa
Sanjay Byrannanavar <sbsa597@gmail.com>
2023-05-21 20:31:14 +0530

    Create basictoprinthetext.ino

---
+++ Programs/Parola basic Program /basictoprinthetext.ino
@@ -0,0 +1,30 @@
+
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+// Define the number of devices we have in the chain and the hardware interface
+// NOTE: These pin numbers will probably not work with your hardware and may
+// need to be adapted
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+
+#define CLK_PIN   18
+#define DATA_PIN  19
+#define CS_PIN    5
+
+// Hardware SPI connection
+// MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// Arbitrary output pins
+MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+void setup(void)
+{ 
+  P.begin();
+}
+
+void loop(void)
+{
+  if (P.displayAnimate())
+    P.displayText("Hello", PA_CENTER, P.getSpeed(), P.getPause(), PA_SCROLL_DOWN, PA_SCROLL_UP);
+}

45ad57e86b7b9e210cd782ae057db58688cf77b6
Sanjay Byrannanavar <sbsa597@gmail.com>
2023-05-21 20:37:28 +0530

    small changes

--- Programs/Parola basic Program /basictoprinthetext.ino
+++ Programs/Parola basic Program /basictoprinthetext.ino
@@ -1,4 +1,9 @@
+// this will print the default speed and pause of max7219 based led matrix
+// for our module FC16_HW we have this
+// speed : 10
+// pause : 100
 
+// this program has updown animation thus make the bounce animation
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
@@ -19,12 +24,19 @@
 MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
 void setup(void)
-{ 
+{   Serial.begin(115200);
   P.begin();
+  int getpause = P.getPause();
+  int getspeed = P.getSpeed();
+  Serial.print("Speed : ");
+  Serial.println(getspeed);
+  Serial.print("Pause : ");
+  Serial.println(getpause);
 }
 
 void loop(void)
 {
   if (P.displayAnimate())
-    P.displayText("Hello", PA_CENTER, P.getSpeed(), P.getPause(), PA_SCROLL_DOWN, PA_SCROLL_UP);
+    P.displayText("hello how are u", PA_CENTER, P.getSpeed(), P.getPause(), PA_SCROLL_DOWN, PA_SCROLL_UP);
+
 }

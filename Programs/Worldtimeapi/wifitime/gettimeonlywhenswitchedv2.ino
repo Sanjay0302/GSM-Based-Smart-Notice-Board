c3462b34916ece6aa7308011d59562470d87a1e4
Sanjay Byrannanavar <sbsa597@gmail.com>
2023-05-21 19:57:55 +0530

    reduced no. of serial print statment by concatinating to string

--- Programs/Worldtimeapi/wifitime/gettimeonlywhenswitchedv2.ino
+++ Programs/Worldtimeapi/wifitime/gettimeonlywhenswitchedv2.ino
@@ -1,31 +1,30 @@
 // only changed the fuction() call in void loop
 
-  #include <WiFi.h>
-  #include <HTTPClient.h>
-  #include <ArduinoJson.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
-  const char* ssid = "YOUR_WIFI_SSID";
-  const char* password = "YOUR_WIFI_PASSWORD";
-  const char* apiUrl = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
-  const int switchPin = 25;
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* apiUrl = "http://worldtimeapi.org/api/timezone/Asia/Kolkata";
+const int switchPin = 25;
+int previousMinute = -1; // Variable to store the previous minute value
 
-  int previousMinute = -1; // Variable to store the previous minute value
+void setup() {
+  Serial.begin(115200);
 
-  void setup() {
-    Serial.begin(115200);
-
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(1000);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
       Serial.println("WiFi...");
-    }
-
-    Serial.println("Connected to WiFi!");
-    pinMode(switchPin, INPUT_PULLUP); // Set the switch pin as input with pull-up resistor enabled
   }
 
-  void loop() {
-    // Check if the switch is pressed (switch pin reads LOW due to the pull-up resistor)
+  Serial.println("Connected to WiFi!");
+    pinMode(switchPin, INPUT_PULLUP); // Set the switch pin as input with pull-up resistor enabled
+}
+
+void loop() {
+     // Check if the switch is pressed (switch pin reads LOW due to the pull-up resistor)
     if (digitalRead(switchPin) == LOW) {
       fetchtime();
     }
@@ -36,50 +35,44 @@
   void fetchtime(){
 // no need to check the wifi status 
 
-        HTTPClient http;
+HTTPClient http;
 
-        http.begin(apiUrl);
-        int httpResponseCode = http.GET();
+    http.begin(apiUrl);
+    int httpResponseCode = http.GET();
 
-        if (httpResponseCode == HTTP_CODE_OK) {
-          String response = http.getString();
-          DynamicJsonDocument json(1024);
-          deserializeJson(json, response);
+    if (httpResponseCode == HTTP_CODE_OK) {
+      String response = http.getString();
+      DynamicJsonDocument json(1024);
+      deserializeJson(json, response);
 
-          const char* currentTime = json["datetime"].as<const char*>();
+      const char* currentTime = json["datetime"].as<const char*>();
 
-          // Extract hours and minutes from the time string
-          int hours = atoi(currentTime + 11);
-          int minutes = atoi(currentTime + 14);
+      // Extract hours and minutes from the time string
+      int hours = atoi(currentTime + 11);
+      int minutes = atoi(currentTime + 14);
 
-          // Convert 24-hour format to 12-hour format
-          int am_pm = hours >= 12 ? 1 : 0;
-          hours = hours % 12;
-          if (hours == 0) {
-            hours = 12;
-          }
+      // Convert 24-hour format to 12-hour format
+      int am_pm = hours >= 12 ? 1 : 0;
+      hours = hours % 12;
+      if (hours == 0) {
+        hours = 12;
+      }
 
-          // Check if the minute value has changed
-          if (minutes != previousMinute) {
-            Serial.print("Current time: ");
-            Serial.print(hours);
-            Serial.print(":");
-            if (minutes < 10) {
-              Serial.print("0");
-            }
-            Serial.print(minutes);
-            Serial.print(" ");
-            Serial.println(am_pm ? "PM" : "AM");
+      // Check if the minute value has changed
+      if (minutes != previousMinute) {
+        String timeString = String(hours) + ":" + (minutes < 10 ? "0" : "") + String(minutes) + " " + (am_pm ? "PM" : "AM");
+        Serial.print("Current time: ");
+        Serial.println(timeString);
 
-            // Update the previousMinute variable
-            previousMinute = minutes;
-          }
-        }
-        else {
-          Serial.print("HTTP request failed with error code: ");
-          Serial.println(httpResponseCode);
-        }
+        // Update the previousMinute variable
+        previousMinute = minutes;
+      }
+    }
+    else {
+      Serial.print("HTTP request failed with error code: ");
+      Serial.println(httpResponseCode);
+    }
 
-        http.end();
-      
-  }
+    http.end();
+
+}
